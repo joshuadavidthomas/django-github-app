29821a06fc31439e28bc155d48bd782b88154cfa
Josh <josh@joshthomas.dev>
2024-11-13 23:35:35 -0600

    tweak sequence generator

--- tests/utils.py
+++ tests/utils.py
@@ -11,17 +11,18 @@ class SequenceGenerator:
         self._counter = 1
 
     def __new__(cls):
-        with cls._lock:
-            if cls._instance is None:
-                cls._instance = super().__new__(cls)
-                cls._instance._counter = 1
-            return cls._instance
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+                    cls._instance._counter = 1
+        return cls._instance
 
     def next(self):
         with self._lock:
             current = self._counter
             self._counter += 1
-            return current
+        return current
 
 
 seq = SequenceGenerator()
