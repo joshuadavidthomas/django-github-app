b1c1af17d358e922d90d03a6603b251f53000227
Josh <josh@joshthomas.dev>
2024-11-16 15:41:44 -0600

    add tests for integration plugin

--- tests/integration/test_plugin.py
+++ tests/integration/test_plugin.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 import pytest
 
+from tests.plugins.integration import CLI_FLAG
 
-def test_automatically_skip():
-    pytest.fail("tests in `integration` directory should be automatically skipped")
+
+def test_automatically_skip(request):
+    if not request.config.getoption(CLI_FLAG):
+        pytest.fail("tests in `integration` directory should be automatically skipped")
