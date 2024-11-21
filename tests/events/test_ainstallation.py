c2ba4b8f06b4aeb06eb69f262f21a832b7e78a9a
Josh <josh@joshthomas.dev>
2024-11-21 12:06:16 -0600

    rename functions

--- tests/events/test_ainstallation.py
+++ tests/events/test_ainstallation.py
@@ -5,11 +5,11 @@ from asgiref.sync import sync_to_async
 from gidgethub.abc import sansio
 from model_bakery import baker
 
-from django_github_app.events.ainstallation import create_installation
-from django_github_app.events.ainstallation import delete_installation
-from django_github_app.events.ainstallation import sync_installation_data
-from django_github_app.events.ainstallation import sync_installation_repositories
-from django_github_app.events.ainstallation import toggle_installation_status
+from django_github_app.events.ainstallation import acreate_installation
+from django_github_app.events.ainstallation import adelete_installation
+from django_github_app.events.ainstallation import async_installation_data
+from django_github_app.events.ainstallation import async_installation_repositories
+from django_github_app.events.ainstallation import atoggle_installation_status
 from django_github_app.models import Installation
 from django_github_app.models import InstallationStatus
 from django_github_app.models import Repository
@@ -18,7 +18,7 @@ from tests.utils import seq
 pytestmark = [pytest.mark.asyncio, pytest.mark.django_db]
 
 
-async def test_create_installation(
+async def test_acreate_installation(
     installation_id, repository_id, override_app_settings
 ):
     data = {
@@ -33,7 +33,7 @@ async def test_create_installation(
     event = sansio.Event(data, event="installation", delivery_id="1234")
 
     with override_app_settings(APP_ID=str(data["installation"]["app_id"])):
-        await create_installation(event, None)
+        await acreate_installation(event, None)
 
     installation = await Installation.objects.aget(
         installation_id=data["installation"]["id"]
@@ -42,7 +42,7 @@ async def test_create_installation(
     assert installation.data == data["installation"]
 
 
-async def test_delete_installation(ainstallation):
+async def test_adelete_installation(ainstallation):
     installation = await ainstallation
     data = {
         "installation": {
@@ -51,7 +51,7 @@ async def test_delete_installation(ainstallation):
     }
     event = sansio.Event(data, event="installation", delivery_id="1234")
 
-    await delete_installation(event, None)
+    await adelete_installation(event, None)
 
     assert not await Installation.objects.filter(
         installation_id=data["installation"]["id"]
@@ -65,7 +65,7 @@ async def test_delete_installation(ainstallation):
         (InstallationStatus.INACTIVE, "unsuspend", InstallationStatus.ACTIVE),
     ],
 )
-async def test_toggle_installation_status_suspend(
+async def test_atoggle_installation_status_suspend(
     status, action, expected, ainstallation
 ):
     installation = await ainstallation
@@ -82,13 +82,13 @@ async def test_toggle_installation_status_suspend(
 
     assert installation.status != expected
 
-    await toggle_installation_status(event, None)
+    await atoggle_installation_status(event, None)
 
     await installation.arefresh_from_db()
     assert installation.status == expected
 
 
-async def test_sync_installation_data(ainstallation):
+async def test_async_installation_data(ainstallation):
     installation = await ainstallation
 
     data = {
@@ -100,13 +100,13 @@ async def test_sync_installation_data(ainstallation):
 
     assert installation.data != data
 
-    await sync_installation_data(event, None)
+    await async_installation_data(event, None)
 
     await installation.arefresh_from_db()
     assert installation.data == data["installation"]
 
 
-async def test_sync_installation_repositories(ainstallation):
+async def test_async_installation_repositories(ainstallation):
     installation = await ainstallation
     existing_repo = await sync_to_async(baker.make)(
         "django_github_app.Repository",
@@ -140,7 +140,7 @@ async def test_sync_installation_repositories(ainstallation):
         repository_id=data["repositories_added"][0]["id"]
     ).aexists()
 
-    await sync_installation_repositories(event, None)
+    await async_installation_repositories(event, None)
 
     assert not await Repository.objects.filter(
         repository_id=data["repositories_removed"][0]["id"]
